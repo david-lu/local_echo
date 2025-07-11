b499c1b8980ec52b8051f76a634aac996b226bd7
David Lu <david-lu@hotmail.com>
2025-07-11 11:22:47 -0700

    Create type.tsx

---
+++ src/type.tsx
@@ -0,0 +1,60 @@
+interface Timeline {
+    audio_track: AudioClip[];
+    visual_track: VisualClip[];
+}
+
+interface BaseClip {
+    start_ms: number;
+    end_ms: number;
+    speaker?: string
+}
+
+interface AudioGenerationParams {
+    speaker: string;
+    text: string;
+    speed: number;
+    stability: number;
+}
+
+interface AudioClip extends BaseClip {
+    type: "audio";
+    audio_generation_params?: AudioGenerationParams;
+    audio_task_id?: string;
+    audio_asset_id?: string;
+}
+
+type ImageGenerationType = "text_to_image" | "image_to_image";
+
+interface ImageGenerationParams {
+    type: ImageGenerationType;
+    ai_model_id: string;
+    prompt: string;
+    aspect_ratio: string;
+}
+
+interface ImageToImageGenerationParams extends ImageGenerationParams {
+    type: "image_to_image";
+    reference_image_asset_id: string;
+}
+
+interface TextToImageGenerationParams extends ImageGenerationParams {
+    type: "text_to_image";
+}
+
+interface VideoGenerationParams {
+    type: "video";
+    ai_model_id: string;
+    description: string;
+    aspect_ratio: string;
+}
+
+interface VisualClip extends BaseClip {
+    type: "visual";
+    image_generation_params?: TextToImageGenerationParams | ImageToImageGenerationParams;
+    image_task_id?: string;
+    image_asset_id?: string;
+
+    video_generation_params?: VideoGenerationParams;
+    video_task_id?: string;
+    video_asset_id?: string;
+}
